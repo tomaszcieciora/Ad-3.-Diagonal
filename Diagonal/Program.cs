d5e4bb8209bbd50f21a75d00ce4e052b7aa84321
Tomasz Ciecióra <tomek.cieciora@gmail.com>
2021-09-24 21:38:15 +0200

    Add project files.

---
+++ Diagonal/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Diagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ad 3. DDiagonal
+
+            Console.WriteLine("Enter the lenght");
+            double lenght = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the width");
+            double width = double.Parse(Console.ReadLine());
+            double diagonal1 = (int) Math.Pow(lenght, 2);
+            double diagonal2 = (int) Math.Pow(width, 2);
+            double element = Math.Sqrt((diagonal1 + diagonal2));
+            Console.WriteLine("Squared lenght="+ diagonal1);
+            Console.WriteLine("Squared width="+ diagonal2); 
+            Console.WriteLine("Diagonal="+ element);
+
+        }
+
+       
+    
+    }
+}
